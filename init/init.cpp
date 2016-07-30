9bb738b7241791ff06916f52a6a3722a104ba4e3
Christopher N. Hesse <raymanfx@gmail.com>
2016-07-30 18:34:14 +0200

    init: Boot in selinux permissive mode

--- init/init.cpp
+++ init/init.cpp
@@ -493,6 +493,7 @@ static void selinux_init_all_handles(void)
     sehandle_prop = selinux_android_prop_context_handle();
 }
 
+#if 0
 enum selinux_enforcing_status { SELINUX_PERMISSIVE, SELINUX_ENFORCING };
 
 static selinux_enforcing_status selinux_status_from_cmdline() {
@@ -506,13 +507,18 @@ static selinux_enforcing_status selinux_status_from_cmdline() {
 
     return status;
 }
+#endif
 
 static bool selinux_is_enforcing(void)
 {
+#if 0
     if (ALLOW_PERMISSIVE_SELINUX) {
         return selinux_status_from_cmdline() == SELINUX_ENFORCING;
     }
     return true;
+#else
+    return false;
+#endif
 }
 
 int selinux_reload_policy(void)
